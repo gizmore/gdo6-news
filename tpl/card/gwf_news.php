31dfb0ddab314ba51853a836af0bfac4739d6649
gizmore <gizmore@wechall.net>
2017-12-31 02:32:42 +0100

    6.07_layout

--- tpl/card/gwf_news.php
+++ tpl/card/gwf_news.php
@@ -1,44 +1,46 @@
 <?php /** @var $gdo \GDO\News\GDO_News **/
+use GDO\Profile\GDT_ProfileLink;
 use GDO\UI\GDT_Button;
 use GDO\UI\GDT_IconButton;
 use GDO\UI\GDT_Link;
 use GDO\User\GDO_User;
 use GDO\UI\GDT_Menu;
+use GDO\UI\GDT_Card;
+use GDO\UI\GDT_HTML;
+use GDO\UI\WithHTML;
 
 $user = GDO_User::current();
 $comments = $gdo->gdoCommentTable();
-?>
-<md-card flex layout-fill class="md-whiteframe-8dp">
-  <md-card-title>
-    <md-card-title-text>
-      <div class="md-headline">
-        <div>“<?= html($gdo->getTitle()); ?>” - <?= $gdo->getCreator()->renderCell(); ?></div>
-        <div class="gdo-card-date"><?= t($gdo->getCreateDate()); ?></div>
-      </div>
-    </md-card-title-text>
-  </md-card-title>
-  <gdo-div></gdo-div>
-  <md-card-content flex>
-    <?= $gdo->displayMessage(); ?>
-  </md-card-content>
-  <gdo-div></gdo-div>
-  <md-card-actions layout="row" layout-align="end center">
-<?php
-$menu = GDT_Menu::make();
+
+$card = GDT_Card::make('news');
+
+$avatar = GDT_ProfileLink::make()->forUser($user)->withNickname()->render();
+$date = tt($gdo->getCreateDate());
+$titleText = $gdo->getTitle();
+$title=<<<EOT
+<div>
+<div>{$avatar}</div>
+<div>{$date}</div>
+</div>
+EOT;
+$card->title($title);
+
+$card->addField(GDT_HTML::withHTML("$titleText<br/>".$gdo->displayMessage()));
+
+
 if ($gdo->canEdit($user))
 {
-	$menu->addField(GDT_IconButton::make()->href(href('News', 'Write', '&id='.$gdo->getID()))->icon('edit'));
+	$card->actions()->addField(GDT_IconButton::make()->href(href('News', 'Write', '&id='.$gdo->getID()))->icon('edit'));
 }
+
 if ($gdo->gdoCommentsEnabled())
 {
     $count = $gdo->getCommentCount();
-    $menu->addField(GDT_Link::make('link_comments')->label('link_comments', [$count])->icon('feedback')->href(href('News', 'Comments', '&id='.$gdo->getID())));
+    $card->actions()->addField(GDT_Link::make('link_comments')->label('link_comments', [$count])->icon('feedback')->href(href('News', 'Comments', '&id='.$gdo->getID())));
     if ($gdo->gdoCanComment($user))
     {
-    	$menu->addField(GDT_Button::make('btn_write_comment')->href(href('News', 'WriteComment', '&id='.$gdo->getID()))->icon('reply'));
+    	$card->actions()->addField(GDT_Button::make('btn_write_comment')->href(href('News', 'WriteComment', '&id='.$gdo->getID()))->icon('reply'));
     }
 }
-echo $menu->render();
-?>
-  </md-card-actions>
-</md-card>
+
+echo $card->render();
