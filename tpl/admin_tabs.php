d11a8f6f54690f33c81d807684b5b6ef5d527449
gizmore <gizmore@wechall.net>
2020-10-14 12:32:14 +0200

    6.10-r9048_bootstrap_theme

--- tpl/admin_tabs.php
+++ tpl/admin_tabs.php
@@ -4,7 +4,7 @@ use GDO\UI\GDT_Link;
 
 $bar = GDT_Bar::make()->horizontal();
 $bar->addFields(array(
-	GDT_Link::make('link_overview')->href(href('News', 'Admin')),
+	GDT_Link::make('btn_overview')->href(href('News', 'Admin')),
 	GDT_Link::make('link_write_news')->href(href('News', 'Write')),
 	GDT_Link::make('link_newsletters')->href(href('News', 'Newsletters')),
 ));
